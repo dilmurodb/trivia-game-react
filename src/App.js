f089d739605a29b3dffe4c2efda73b35fddf6c3c
bukharov <dilmurod.bukharov@gmail.com>
2021-10-14 17:42:11 -0400

    created links in header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter, router } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/">Wello World</Link>
+      <ul>
+        <li>
+          <Link to="/art">Art</Link>
+        </li>
+        <li>
+          <Link to="/books">Books</Link>
+        </li>
+        <li>
+          <Link to="/geography">Geography</Link>
+        </li>
+        <li>
+          <Link to="/mythology">Mythology</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
